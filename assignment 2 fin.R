8fac695b3a04c0a96bf1658e9552ae7d5670b164
haleylam0704 <haleylam0704@gmail.com>
2023-04-04 02:12:02 -0500

    edit final draft - made a function that can compute its inverse (makeCacheMatrix()) and a function to solve the inverse (cacheSolve())

---
+++ assignment 2 fin.R
@@ -0,0 +1,31 @@
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        cacheInverse <- function() {
+        if (!is.null(inv))       {
+               return(inv)
+        }
+                inv <- solve(x)
+                return(inv) 
+        }
+        list(set = set, get = get,
+            cacheInverse = cacheInverse)
+}
+
+
+cacheSolve <- function(x, ...)
+{
+        inv <- x$cacheInverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
